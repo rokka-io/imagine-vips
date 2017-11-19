f8666e7369bd0ac84d790c2429b7ce52d08318bd
Christian Stocker <me@chregu.tv>
2017-11-19 21:52:39 +0100

    correct copyright stuff

--- lib/Imagine/Vips/Layers.php
+++ lib/Imagine/Vips/Layers.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Imagine package.
  *
- * (c) Bulat Shakirzyanov <mallluhuct@gmail.com>
+ * This file is part of the imagine-vips package.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
