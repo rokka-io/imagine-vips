f6a070760f3dc68f228dfb670832204b9e02baa6
Christian Stocker <me@chregu.tv>
2022-10-12 21:48:20 +0200

    Test on all pushes

--- tests/EnabledTest.php
+++ tests/EnabledTest.php
@@ -14,9 +14,9 @@ final class EnabledTest extends TestCase
 
     public function testCorrectLibrary()
     {
-        $version = \Composer\InstalledVersions::getVersion("jcupitt/vips");
+        $version = \Composer\InstalledVersions::getVersion('jcupitt/vips');
         // if we have version 2.1 of the extension, we need the ffi extension
-        if ( version_compare($version, "2.1.0", ">=")) {
+        if (version_compare($version, '2.1.0', '>=')) {
             $this->assertTrue(\extension_loaded('ffi'), 'The needed ffi extension was not installed');
             // and check we get a string
             $this->assertIsString(\Jcupitt\Vips\Config::version());
@@ -24,7 +24,7 @@ final class EnabledTest extends TestCase
             return;
         }
         // otherwise the vips extension needs to be installed and version needs to be lower than 2.0.0
-        if (version_compare($version, "2.0.0", "<")) {
+        if (version_compare($version, '2.0.0', '<')) {
             $this->assertTrue(\extension_loaded('vips'), 'The needed vips extension was not installed');
             // and check we get a string
             $this->assertIsString(\Jcupitt\Vips\Config::version());
@@ -32,6 +32,6 @@ final class EnabledTest extends TestCase
             return;
         }
         // otherwise we may have an unsupported version
-        $this->fail('jcupitt/vips version '. $version . ' is not supported');
+        $this->fail('jcupitt/vips version '.$version.' is not supported');
     }
 }
